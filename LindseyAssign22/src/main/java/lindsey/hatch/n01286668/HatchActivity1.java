a822ee1540c193db9203770fe36c58d01d893b7f
Lindsey Hatch <n01286668@humbermail.ca>
2021-02-18 22:35:02 -0500

    Created second screen

---
+++ LindseyAssign22/src/main/java/lindsey/hatch/n01286668/HatchActivity1.java
@@ -0,0 +1,14 @@
+package lindsey.hatch.n01286668;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HatchActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hatch1);
+    }
+}
