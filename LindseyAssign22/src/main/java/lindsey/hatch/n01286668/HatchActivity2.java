eb78ecf39b7182a2d8e6b6779c5a1c800397b5bc
Lindsey Hatch <n01286668@humbermail.ca>
2021-02-19 17:11:40 -0500

    Added spinner and spinner values

--- LindseyAssign22/src/main/java/lindsey/hatch/n01286668/HatchActivity2.java
+++ LindseyAssign22/src/main/java/lindsey/hatch/n01286668/HatchActivity2.java
@@ -5,7 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HatchActivity2 extends AppCompatActivity {
-
+    String[] provinces = {"Alberta",
+            "British Columbia",
+            "Manitoba",
+            "New Brunswick",
+            "Northwest Territories",
+            "Nova Scotia",
+            "Nunavut",
+            "Ontario",
+            "Prince Edward Island",
+            "Quebec",
+            "Saskatchewan",
+            "Yukon"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
