0591c462b2a2740873eecc5da6b06566b8d5ab81
Lindsey Hatch <n01286668@humbermail.ca>
2021-02-18 22:33:33 -0500

    set up radio buttons and other layouts. also handling button presses and radio button clicks

--- LindseyAssign22/src/main/java/lindsey/hatch/n01286668/LindseyActivity.java
+++ LindseyAssign22/src/main/java/lindsey/hatch/n01286668/LindseyActivity.java
@@ -2,13 +2,73 @@ package lindsey.hatch.n01286668;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.app.Notification;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
 
 public class LindseyActivity extends AppCompatActivity {
+
+    String store;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getActionBar();
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent;
+                intent = new Intent(LindseyActivity.this, HatchActivity1.class);
+                intent.putExtra("key", store);
+                startActivity(intent);
+                setContentView(R.layout.activity_hatch1);
+            }
+        });
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton rb1 = (RadioButton) findViewById(R.id.radioButton1);
+                RadioButton rb2 = (RadioButton) findViewById(R.id.radioButton2);
+                RadioButton rb3 = (RadioButton) findViewById(R.id.radioButton3);
+                if (rb1.isChecked()) {
+                    store = "Pizza Pizza";
+                }
+                else if(rb2.isChecked()) {
+                    store = "Pizza Hut";
+                }
+                else if(rb3.isChecked()) {
+                    store = "Domino's Pizza";
+                }
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        CreateMenu(menu);
+        return true;
+    }
+
+    private void CreateMenu(Menu menu)
+    {
+        MenuItem mnu1 = menu.add(0, 0, 0, "Item 1");
+        {
+            mnu1.setShowAsAction(
+                    MenuItem.SHOW_AS_ACTION_IF_ROOM |
+                            MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+        }
+
     }
 }
